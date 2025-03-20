2f273d4a2e602804596c27838f2fe1584774329c
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:41:16 -0500

    feat: create VehicleResponseDTO

---
+++ src/main/java/secretaria_transito/project/dto/VehicleResponseDTO.java
@@ -0,0 +1,24 @@
+package secretaria_transito.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import secretaria_transito.project.enums.VehicleType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VehicleResponseDTO {
+
+    private Long vehicleId;
+
+    private String licensePlate;
+
+    private String brand;
+
+    private VehicleType vehicleType;
+
+    private Long ownerId;
+}
