c2402c93d2976d34a004e5ee8d58222c416e5de0
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 11:21:31 -0500

    feat: create TrafficTicketService with get traffic ticket by vehicle method

--- src/main/java/secretaria_transito/project/services/TrafficTicketService.java
+++ src/main/java/secretaria_transito/project/services/TrafficTicketService.java
@@ -1,9 +1,34 @@
 package secretaria_transito.project.services;
 
 import org.springframework.stereotype.Service;
+import secretaria_transito.project.dto.TrafficTicketResponseDTO;
+import secretaria_transito.project.entities.Vehicle;
+import secretaria_transito.project.exceptions.BadCreateRequest;
+import secretaria_transito.project.maps.TrafficTicketMapper;
+import secretaria_transito.project.repositories.TrafficTicketRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TrafficTicketService {
 
+    private final TrafficTicketRepository trafficTicketRepository;
+    private final VehicleService vehicleService;
+    private final TrafficTicketMapper trafficTicketMapper;
 
+    public TrafficTicketService(TrafficTicketRepository trafficTicketRepository, VehicleService vehicleService, TrafficTicketMapper trafficTicketMapper) {
+        this.trafficTicketRepository = trafficTicketRepository;
+        this.vehicleService = vehicleService;
+        this.trafficTicketMapper = trafficTicketMapper;
+    }
+
+    public List<TrafficTicketResponseDTO> getTrafficTicketByVehicleId(Long vehicleId) {
+        Optional<Vehicle> vehicleOptional = vehicleService.getVehicleByVehicleId(vehicleId);
+        if (vehicleOptional.isEmpty()) {
+            throw new BadCreateRequest("El vehiculo no existe.");
+        }
+        Vehicle vehicle = vehicleOptional.get();
+        return trafficTicketMapper.mapTrafficTicketList(trafficTicketRepository.findTrafficTicketByVehicleVehicleId(vehicleId));
+    }
 }
