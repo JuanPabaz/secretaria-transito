cbbf9ef8925b343c4a16acaade9d9e6966f6ad29
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 11:20:06 -0500

    feat: add new attributes to RegisterRequestDTO

--- src/main/java/secretaria_transito/project/dto/RegisterRequestDTO.java
+++ src/main/java/secretaria_transito/project/dto/RegisterRequestDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import secretaria_transito.project.enums.OwnerType;
 import secretaria_transito.project.enums.Role;
 
 @Data
@@ -13,6 +14,9 @@ import secretaria_transito.project.enums.Role;
 public class RegisterRequestDTO {
 
     private String fullName;
+    private OwnerType ownerType;
+    private String idNumber;
+    private String address;
     private String username;
     private String password;
     private Role role;
