abb1dc6fe361c013075f6ab0c445147160110747
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:40:06 -0500

    feat: feat add @builder annotation

--- src/main/java/secretaria_transito/project/entities/Vehicle.java
+++ src/main/java/secretaria_transito/project/entities/Vehicle.java
@@ -1,10 +1,7 @@
 package secretaria_transito.project.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import secretaria_transito.project.enums.VehicleType;
 
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Vehicle {
 
     @Id
