ce3808f55c0119982c8a018c70d5fcaa40f7a1f2
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:52:19 -0500

    delete: unnecessary owner entity

--- src/main/java/secretaria_transito/project/repositories/OwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package secretaria_transito.project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import secretaria_transito.project.entities.Owner;
-
-public interface OwnerRepository extends JpaRepository<Owner,Long> {
-}
