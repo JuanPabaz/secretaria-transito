fd3aaedee122b314ef9027dfdd6b9ce68762ecb9
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:40:31 -0500

    feat: create VehicleController with get vehicle by owner id and save vehicle endpoints

---
+++ src/main/java/secretaria_transito/project/controllers/VehicleController.java
@@ -0,0 +1,30 @@
+package secretaria_transito.project.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import secretaria_transito.project.dto.VehicleRequestDTO;
+import secretaria_transito.project.dto.VehicleResponseDTO;
+import secretaria_transito.project.services.VehicleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicle")
+@CrossOrigin(origins = "*", allowedHeaders = {"Authorization", "Content-Type"})
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping("/{ownerId}")
+    public List<VehicleResponseDTO> getVehicle(@PathVariable Long ownerId) {
+        return vehicleService.getVehicleByOwnerOwnerId(ownerId);
+    }
+
+    @PostMapping
+    public VehicleResponseDTO saveVehicle(@RequestBody VehicleRequestDTO vehicleRequestDTO) {
+        return vehicleService.saveVehicle(vehicleRequestDTO);
+    }
+}
