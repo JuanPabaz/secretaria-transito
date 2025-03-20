92767242da9e43d9b59581da84d48cb9d909a3de
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:40:42 -0500

    feat: create VehicleMapper

---
+++ src/main/java/secretaria_transito/project/maps/VehicleMapper.java
@@ -0,0 +1,31 @@
+package secretaria_transito.project.maps;
+
+import org.springframework.stereotype.Component;
+import secretaria_transito.project.dto.VehicleResponseDTO;
+import secretaria_transito.project.entities.Vehicle;
+
+import java.util.List;
+
+@Component
+public class VehicleMapper {
+
+    public VehicleResponseDTO mapVehicle(Vehicle vehicle) {
+        if (vehicle == null) {
+            return null;
+        }
+        return VehicleResponseDTO.builder()
+                .vehicleId(vehicle.getVehicleId())
+                .vehicleType(vehicle.getVehicleType())
+                .licensePlate(vehicle.getLicensePlate())
+                .brand(vehicle.getBrand())
+                .ownerId(vehicle.getOwner().getOwnerId())
+                .build();
+    }
+
+    public List<VehicleResponseDTO> mapVehicleList(List<Vehicle> vehicleList) {
+        return vehicleList.stream()
+                .map(this::mapVehicle)
+                .toList();
+    }
+
+}
