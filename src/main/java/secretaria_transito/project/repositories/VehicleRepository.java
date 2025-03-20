6d1d049f35c53dba626c1c677c7e446c02d94a48
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:40:58 -0500

    feat: create VehicleRepository with find vehicle by onwer id

---
+++ src/main/java/secretaria_transito/project/repositories/VehicleRepository.java
@@ -0,0 +1,11 @@
+package secretaria_transito.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import secretaria_transito.project.entities.Vehicle;
+
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+
+    List<Vehicle> findVehicleByOwnerOwnerId(Long ownerId);
+}
