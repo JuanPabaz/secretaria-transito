036268277d1c87bb474f97bf1f9bbed6dab9b59d
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 10:54:47 -0500

    change: owner to user

--- src/main/java/secretaria_transito/project/services/VehicleService.java
+++ src/main/java/secretaria_transito/project/services/VehicleService.java
@@ -3,7 +3,7 @@ package secretaria_transito.project.services;
 import org.springframework.stereotype.Service;
 import secretaria_transito.project.dto.VehicleRequestDTO;
 import secretaria_transito.project.dto.VehicleResponseDTO;
-import secretaria_transito.project.entities.Owner;
+import secretaria_transito.project.entities.User;
 import secretaria_transito.project.entities.Vehicle;
 import secretaria_transito.project.exceptions.BadCreateRequest;
 import secretaria_transito.project.maps.VehicleMapper;
@@ -17,29 +17,29 @@ public class VehicleService {
 
     private final VehicleRepository vehicleRepository;
     private final VehicleMapper vehicleMapper;
-    private final OwnerService ownerService;
+    private final UserService userService;
 
-    public VehicleService(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper, OwnerService ownerService) {
+    public VehicleService(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper, UserService userService) {
         this.vehicleRepository = vehicleRepository;
         this.vehicleMapper = vehicleMapper;
-        this.ownerService = ownerService;
+        this.userService = userService;
     }
 
-    public List<VehicleResponseDTO> getVehicleByOwnerOwnerId(Long ownerId) {
-        return vehicleMapper.mapVehicleList(vehicleRepository.findVehicleByOwnerOwnerId(ownerId));
+    public List<VehicleResponseDTO> getVehicleByOwnerOwnerId(Integer ownerId) {
+        return vehicleMapper.mapVehicleList(vehicleRepository.findVehicleByUserIdUser(ownerId));
     }
 
     public VehicleResponseDTO saveVehicle(VehicleRequestDTO vehicleRequestDTO) {
-        Optional<Owner> ownerOptional = ownerService.findOwnerById(vehicleRequestDTO.getOwnerId());
-        if (ownerOptional.isEmpty()) {
+        Optional<User> userOptional = userService.findUserById(vehicleRequestDTO.getUserId());
+        if (userOptional.isEmpty()) {
             throw new BadCreateRequest("No se encontró el propietario.");
         }
-        Owner owner = ownerOptional.get();
+        User user = userOptional.get();
         Vehicle vehicle = Vehicle.builder()
                 .vehicleType(vehicleRequestDTO.getVehicleType())
                 .brand(vehicleRequestDTO.getBrand())
                 .licensePlate(vehicleRequestDTO.getLicensePlate())
-                .owner(ownerOptional.get())
+                .user(user)
                 .build();
 
         return vehicleMapper.mapVehicle(vehicleRepository.save(vehicle));
