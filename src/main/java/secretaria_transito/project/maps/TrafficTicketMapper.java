829857dd695ebe54fc9483f6c76afa30ec479b76
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-20 11:21:02 -0500

    feat: create TrafficTicketMapper

---
+++ src/main/java/secretaria_transito/project/maps/TrafficTicketMapper.java
@@ -0,0 +1,30 @@
+package secretaria_transito.project.maps;
+
+import org.springframework.stereotype.Component;
+import secretaria_transito.project.dto.TrafficTicketResponseDTO;
+import secretaria_transito.project.entities.TrafficTicket;
+
+import java.util.List;
+
+@Component
+public class TrafficTicketMapper {
+
+    public TrafficTicketResponseDTO mapTrafficTicket(TrafficTicket trafficTicket) {
+        if (trafficTicket == null) {
+            return null;
+        }
+        return TrafficTicketResponseDTO.builder()
+                .trafficTicketId(trafficTicket.getTrafficTicketId())
+                .date(trafficTicket.getDate())
+                .trafficAgentId(trafficTicket.getTrafficAgent().getTrafficAgentId())
+                .detectionCameraId(trafficTicket.getDetectionCamera().getCameraId())
+                .description(trafficTicket.getDescription())
+                .build();
+    }
+
+    public List<TrafficTicketResponseDTO> mapTrafficTicketList(List<TrafficTicket> trafficTicketList) {
+        return trafficTicketList.stream()
+                .map(this::mapTrafficTicket)
+                .toList();
+    }
+}
