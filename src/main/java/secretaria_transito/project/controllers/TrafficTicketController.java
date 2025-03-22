7e16f53c8e3668846658990431aa5d9fca94486e
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-21 19:12:53 -0500

    feat: add generateTrafficTicketInvoice endpoint

--- src/main/java/secretaria_transito/project/controllers/TrafficTicketController.java
+++ src/main/java/secretaria_transito/project/controllers/TrafficTicketController.java
@@ -1,13 +1,11 @@
 package secretaria_transito.project.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import secretaria_transito.project.dto.TrafficTicketResponseDTO;
 import secretaria_transito.project.services.TrafficTicketService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/traffic-ticket")
@@ -22,6 +20,13 @@ public class TrafficTicketController {
     @GetMapping("/{idVehicle}")
     public List<TrafficTicketResponseDTO> getTrafficTicket(@PathVariable Long idVehicle) {
         return trafficTicketService.getTrafficTicketByVehicleId(idVehicle);
+    }
+
+    @GetMapping("/report")
+    public Map<String, String> generateTrafficTicketInvoice(@RequestParam Long idVehicle,
+                                                            @RequestParam Long trafficTicketId,
+                                                            @RequestParam Integer userId) {
+        return trafficTicketService.generateInvoicePdf(userId, idVehicle, trafficTicketId);
     }
 
 }
