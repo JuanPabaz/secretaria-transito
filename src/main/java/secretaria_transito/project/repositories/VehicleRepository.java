bfb2f63e5642022a213f9aa01c25e466450280d7
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-23 14:04:57 -0500

    feat: update the find vehicles by user query with the registration relation

--- src/main/java/secretaria_transito/project/repositories/VehicleRepository.java
+++ src/main/java/secretaria_transito/project/repositories/VehicleRepository.java
@@ -1,11 +1,16 @@
 package secretaria_transito.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import secretaria_transito.project.entities.Vehicle;
 
 import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
 
-    List<Vehicle> findVehicleByUserIdUser(Integer userId);
+    @Query("SELECT v FROM Vehicle AS v INNER JOIN Registration as r " +
+            "ON v.registration.registrationId = r.registrationId " +
+            "WHERE r.user.idUser = :userId")
+    List<Vehicle> findVehicleByUser(@Param("userId") Integer userId);
 }
