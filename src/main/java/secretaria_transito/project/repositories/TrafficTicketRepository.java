c9b4e1e2afd3a4a8dcf46d4e0e8d65eda7503d56
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-23 14:23:10 -0500

    feat: add find traffic ticker by user query

--- src/main/java/secretaria_transito/project/repositories/TrafficTicketRepository.java
+++ src/main/java/secretaria_transito/project/repositories/TrafficTicketRepository.java
@@ -1,6 +1,8 @@
 package secretaria_transito.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import secretaria_transito.project.entities.TrafficTicket;
 
 import java.util.List;
@@ -8,4 +10,9 @@ import java.util.List;
 public interface TrafficTicketRepository extends JpaRepository<TrafficTicket, Long> {
 
     List<TrafficTicket> findTrafficTicketByVehicleVehicleId(Long vehicleId);
+
+    @Query("SELECT t FROM TrafficTicket AS t INNER JOIN Vehicle AS v " +
+            "ON t.vehicle.vehicleId = v.vehicleId " +
+            "WHERE v.registration.user.idUser = :userId")
+    List<TrafficTicket> findTrafficTicketByUser(@Param("userId") Integer userId);
 }
