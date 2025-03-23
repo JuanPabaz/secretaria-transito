aa78c4a0140127de70298a7b128a8351e06b21d5
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-23 14:05:16 -0500

    feat: add registration date to VehicleRequestDTO

--- src/main/java/secretaria_transito/project/dto/VehicleRequestDTO.java
+++ src/main/java/secretaria_transito/project/dto/VehicleRequestDTO.java
@@ -1,10 +1,13 @@
 package secretaria_transito.project.dto;
 
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import secretaria_transito.project.enums.VehicleType;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +18,8 @@ public class VehicleRequestDTO {
     private String licensePlate;
 
     private String brand;
+
+    private Date registrationDate;
 
     private VehicleType vehicleType;
 
